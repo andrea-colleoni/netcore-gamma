2cd558e89d23d0539679a51bf585905bddf01989
Andrea Colleoni <andrea@colleoni.info>
2021-04-29 14:24:59 +0200

    heroes api

---
+++ Angular/angular-visual-studio/HeroesAPI/Context/HeroesDb.cs
@@ -0,0 +1,28 @@
+﻿using HeroesAPI.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HeroesAPI.Context
+{
+    public class HeroesDb: DbContext
+    {
+        public HeroesDb(DbContextOptions<HeroesDb> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Hero> Heroes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Hero>()
+                .HasKey(e => e.Id);
+        }
+
+    }
+}
---
+++ Angular/angular-visual-studio/HeroesAPI/Model/Hero.cs
@@ -0,0 +1,9 @@
+﻿namespace HeroesAPI.Model
+{
+    public class Hero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
