d2f0a8a5e0bf05e462d2052ff498122fabad7c4a
Andrea Colleoni <andrea@colleoni.info>
2021-04-30 17:42:59 +0200

    fine gg 3

--- Angular/blog-ng/src/app/app.module.ts
+++ Angular/blog-ng/src/app/app.module.ts
@@ -1,6 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { TooltipModule } from 'primeng/tooltip';
 
 import { AppComponent } from './app.component';
 import { PostsComponent } from './components/posts/posts.component';
@@ -8,7 +15,7 @@ import { PostDetailComponent } from './components/post-detail/post-detail.compon
 import { AppRoutingModule } from './app-routing.module';
 import { CommentiComponent } from './components/commenti/commenti.component';
 import { CommentoComponent } from './components/commento/commento.component';
-import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -19,9 +26,19 @@ import { ReactiveFormsModule } from '@angular/forms';
     CommentoComponent
   ],
   imports: [
+    // angular
     BrowserModule,
+    BrowserAnimationsModule,
     ReactiveFormsModule,
     HttpClientModule,
+
+    // prime
+    ButtonModule,
+    InputTextModule,
+    TableModule,
+    TooltipModule,
+
+    // mia app
     AppRoutingModule,
   ],
   providers: [],
--- Angular/blog-ng/src/app/components/posts/posts.component.ts
+++ Angular/blog-ng/src/app/components/posts/posts.component.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 import { Post } from 'src/app/model/post';
 import { PostService } from 'src/app/services/post.service';
 
+import { jsPDF } from "jspdf";
+
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
@@ -13,12 +15,23 @@ export class PostsComponent implements OnInit {
   posts: Post[];
   selectedPost: Post;
 
+  // colonne della prime table
+  cols: any[];
+  exportColumns: any[];
+
   constructor(
     private ps: PostService,
     private router: Router,
   ) { }
 
   ngOnInit(): void {
+    this.cols = [
+      { field: 'id', header: 'ID' },
+      { field: 'userId', header: 'ID Utente' },
+      { field: 'title', header: 'Titolo del post' }
+    ];
+
+    this.exportColumns = this.cols.map(col => ({title: col.header, dataKey: col.field}));
     this.ps.getPosts().subscribe(p => this.posts = p);
   }
 
@@ -27,4 +40,30 @@ export class PostsComponent implements OnInit {
     this.selectedPost = post;
   }
 
+  // exportPdf() {
+  //   let doc = new jsPDF('l', 'pt');
+  //   doc.autoTable(this.exportColumns, this.posts);
+  //   doc.save('products.pdf'); 
+  // }
+
+
+  exportExcel() {
+    import("xlsx").then(xlsx => {
+        const worksheet = xlsx.utils.json_to_sheet(this.posts);
+        const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+        const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+        this.saveAsExcelFile(excelBuffer, "posts");
+    });
+  }
+
+  saveAsExcelFile(buffer: any, fileName: string): void {
+    import("file-saver").then(FileSaver => {
+        let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+        let EXCEL_EXTENSION = '.xlsx';
+        const data: Blob = new Blob([buffer], {
+            type: EXCEL_TYPE
+        });
+        FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+    });
+  }
 }
